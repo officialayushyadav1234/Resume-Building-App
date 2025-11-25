e7725e0ba5f4500f13ea0a895696a15f43f8de03
officialayushyadav1234 <officialayushyadav1234@gmail.com>
2025-11-26 03:25:10 +0530

    Updated Code

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function printpdf() {
     var content = document.getElementById("resume");
-  
+
+    // 1. Hide buttons and checkboxes
     const allButtons = document.querySelectorAll("#print button");
     allButtons.forEach(button => {
         button.classList.add("none");
@@ -8,199 +9,40 @@ function printpdf() {
     let allInputCheckboxes = document.querySelectorAll(".input-checkbox");
     allInputCheckboxes.forEach(input => {
         input.classList.add("none");
-    })
-  
-  allButtons.forEach(button => {
-      button.classList.remove("none");
-  })
-  allInputCheckboxes.forEach(input => {
-      input.classList.remove("none");
-  })
-  
-    html2pdf(content, {
-        html2canvas: { scale: 1, logging: true, dpi: 500 }
     });
-  }
-  
-  function addedu() {
-    const head = document.createElement('div');
-    document.getElementById("education").appendChild(head);
-    head.innerHTML = ('<div class="edublock"><span><input type="checkbox" class="input-checkbox"></span><span class="education-head" contenteditable="true">YOUR DEGREE</span><div ><span contenteditable="true">Institute name</span> - <span contenteditable="true">Passing Year</span></div></div>');
-    saveresume();
-  }
-  function remedu(event) {
-    let val = 0;
-    let empty = true;
-    const allInputCheckboxes = event.target.parentElement.getElementsByClassName("input-checkbox");
-    const array = Array.from(allInputCheckboxes);
-    if (array.length === 0) {
-        alert("No fields are present to be deleted!")
+
+    // 2. TEMPORARY: Force Desktop Width for PDF Generation
+    // We save the current width to restore it later
+    const originalWidth = content.style.width;
+    const originalMargin = content.style.margin;
+    
+    // Force A4 dimensions for the snapshot
+    if (window.innerWidth < 850) {
+        content.style.width = "800px";
+        content.style.margin = "0 auto"; 
     }
-    else {
-        console.log(array);
-        array.forEach(element => {
-            if (element.checked === true) {
-                val = 1;
-                element.parentElement.parentElement.remove();
-            }
-        })
-        if (val === 0) alert("Please select the checkboxes to delete the required field!")
-    }
-    saveresume();
-  }
-  
-  
-  function addskill() {
-    const head = document.createElement('div');
-    document.getElementById("skills").appendChild(head);
-    head.innerHTML = ('<div class="skill"><span><input type="checkbox" class="input-checkbox"></span><span><i class="fas fa-chevron-circle-right"></i></span>   <span contenteditable="true">write your skill here</span></div>');
-    saveresume();
-  }
-  
-  function remskill(event) {
-    let val = 0;
-    const allInputCheckboxes = event.target.parentElement.getElementsByClassName("input-checkbox");
-    const array = Array.from(allInputCheckboxes);
-    if (array.length === 0) {
-        alert("No fields are present to be deleted!")
-    }
-    else {
-        console.log(array);
-        array.forEach(element => {
-            if (element.checked === true) {
-                val = 1;
-                element.parentElement.parentElement.remove();
-            }
-        })
-        if (val === 0) alert("Please select the checkboxes to delete the required field!")
-    }
-    saveresume();
-  }
-  
-  
-  function addLang() {
-    const head = document.createElement('div');
-    document.getElementById("languages").appendChild(head);
-    head.innerHTML = ('<div class="language"><span><input type="checkbox" class="input-checkbox"></span><span contenteditable="true">LANGNAME</span> - <span contenteditable="true">level u know</span></div>');
-    saveresume();
-  }
-  function remLang(event) {
-    let val = 0;
-    const allInputCheckboxes = event.target.parentElement.getElementsByClassName("input-checkbox");
-    const array = Array.from(allInputCheckboxes);
-    if (array.length === 0) {
-        alert("No fields are present to be deleted!")
-    }
-    else {
-        console.log(array);
-        array.forEach(element => {
-            if (element.checked === true) {
-                val = 1;
-                element.parentElement.parentElement.remove();
-            }
-        })
-        if (val === 0) alert("Please select the checkboxes to delete the required field!")
-    }
-    saveresume();
-  }
-  
-  
-  function addAch() {
-    const head = document.createElement('div');
-    document.getElementById("achievement").appendChild(head);
-    head.innerHTML = ('<div class="achieve" ><span><input type="checkbox" class="input-checkbox"></span><span contenteditable="true">Write your achievement</span></div>');
-    saveresume();
-  }
-  function remAch(event) {
-    let val = 0;
-    const allInputCheckboxes = event.target.parentElement.getElementsByClassName("input-checkbox");
-    const array = Array.from(allInputCheckboxes);
-    if (array.length === 0) {
-        alert("No fields are present to be deleted!")
-    }
-    else {
-        console.log(array);
-        array.forEach(element => {
-            if (element.checked === true) {
-                val = 1;
-                element.parentElement.parentElement.remove();
-            }
-        })
-        if (val === 0) alert("Please select the checkboxes to delete the required field!")
-    }
-    saveresume();
-  }
-  
-  
-  function addInt() {
-    const head = document.createElement('div');
-    document.getElementById("interest").appendChild(head);
-    head.innerHTML = ('<div class="achieve" ><span><input type="checkbox" class="input-checkbox"></span><span contenteditable="true">Write interest</span></div>');
-    saveresume();
-  }
-  function remInt(event) {
-    let val = 0;
-    const allInputCheckboxes = event.target.parentElement.getElementsByClassName("input-checkbox");
-    const array = Array.from(allInputCheckboxes);
-    if (array.length === 0) {
-        alert("No fields are present to be deleted!")
-    }
-    else {
-        array.forEach(element => {
-            if (element.checked === true) {
-                val = 1;
-                element.parentElement.parentElement.remove();
-            }
-        })
-        if (val === 0) alert("Please select the checkboxes to delete the required field!")
-    }
-    saveresume();
-  }
-  
-  let maxNewSection = 1;
-  function addsec() {
-    if (maxNewSection < 2) {
-        const head = document.createElement('div');
-        document.getElementById("newsec").appendChild(head);
-        if (maxNewSection === 0) {
-            head.innerHTML = ('<div><span><input type="checkbox" class="input-checkbox"></span><span class="title" contenteditable="true">NEW SECTION</span><br><br><div contenteditable="true">This is the description part of your new section. Try to stay within limit and write something which has less than 400 characters. The spaces and symbols you use will also be included so use them for an indentation effect.</div></div>');
+
+    // 3. Generate PDF
+    var opt = {
+        margin:       0,
+        filename:     'myResume.pdf',
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { scale: 2 }, // Higher scale for better clarity
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    html2pdf().from(content).set(opt).save().then(() => {
+        // 4. RESTORE: Bring back the mobile view and buttons after download
+        if (window.innerWidth < 850) {
+            content.style.width = originalWidth;
+            content.style.margin = originalMargin;
         }
-        else {
-            head.innerHTML = ('<div><br><br><span><input type="checkbox" class="input-checkbox"></span><span class="title" contenteditable="true">NEW SECTION</span><br><br><div contenteditable="true">This is the description part of your new section. Try to stay within limit and write something which has less than 400 characters. The spaces and symbols you use will also be included so use them for an indentation effect.</div></div>');
-        }
-  
-        maxNewSection = maxNewSection + 1;
-    }
-    else {
-        alert("Atmost 2 NEW SECTION can be added!")
-  
-    }
-    saveresume();
-  }
-  function remsec(event) {
-    let val = 0;
-    const allInputCheckboxes = event.target.parentElement.getElementsByClassName("input-checkbox");
-    const array = Array.from(allInputCheckboxes);
-    if (array.length === 0) {
-        alert("No fields are present to be deleted!")
-    }
-    else {
-        console.log(array);
-        array.forEach(element => {
-            if (element.checked === true) {
-                val = 1;
-                maxNewSection = maxNewSection - 1;
-                element.parentElement.parentElement.remove();
-            }
-        })
-        if (val === 0) alert("Please select the checkboxes to delete the required field!")
-    }
-    saveresume();
-  }
-  
-  function saveresume() {
-    var sec = document.getElementById("print");
-    value1 = sec.innerHTML;
-    var info = document.getElementById("custinfo");
-    info.value = value1;
-  }
+
+        allButtons.forEach(button => {
+            button.classList.remove("none");
+        });
+        allInputCheckboxes.forEach(input => {
+            input.classList.remove("none");
+        });
+    });
+}
